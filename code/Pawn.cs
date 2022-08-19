c832b72c95351e58361b9ea5857f0f54d9d53324
ogniK <25727384+ogniK5377@users.noreply.github.com>
2022-08-20 00:50:56 +1000

    Initial commit

---
+++ code/Pawn.cs
@@ -0,0 +1,66 @@
+﻿using Sandbox;
+using System;
+using System.Linq;
+
+namespace GoBack;
+
+partial class Pawn : AnimatedEntity
+{
+	/// <summary>
+	/// Called when the entity is first created 
+	/// </summary>
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		//
+		// Use a watermelon model
+		//
+		SetModel( "models/sbox_props/watermelon/watermelon.vmdl" );
+
+		EnableDrawing = true;
+		EnableHideInFirstPerson = true;
+		EnableShadowInFirstPerson = true;
+	}
+
+	/// <summary>
+	/// Called every tick, clientside and serverside.
+	/// </summary>
+	public override void Simulate( Client cl )
+	{
+		base.Simulate( cl );
+
+		Rotation = Input.Rotation;
+		EyeRotation = Rotation;
+
+		// build movement from the input values
+		var movement = new Vector3( Input.Forward, Input.Left, 0 ).Normal;
+
+		// rotate it to the direction we're facing
+		Velocity = Rotation * movement;
+
+		// apply some speed to it
+		Velocity *= Input.Down( InputButton.Run ) ? 1000 : 200;
+
+		// apply it to our position using MoveHelper, which handles collision
+		// detection and sliding across surfaces for us
+		MoveHelper helper = new MoveHelper( Position, Velocity );
+		helper.Trace = helper.Trace.Size( 16 );
+		if ( helper.TryMove( Time.Delta ) > 0 )
+		{
+			Position = helper.Position;
+		}
+	}
+
+	/// <summary>
+	/// Called every frame on the client
+	/// </summary>
+	public override void FrameSimulate( Client cl )
+	{
+		base.FrameSimulate( cl );
+
+		// Update rotation every frame, to keep things smooth
+		Rotation = Input.Rotation;
+		EyeRotation = Rotation;
+	}
+}
